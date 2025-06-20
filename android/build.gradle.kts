e473196e60074f139c75b1d2db3754fe95f8c799
ch-66 <771923501@qq.com>
2025-06-20 11:33:35 +0800

    移除Android构建配置中的buildscript部分，简化Gradle配置文件

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,21 +2,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-buildscript {
-    ext {
-        kotlin_version = "1.8.22"
-        agp_version = "7.3.0"
-    }
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
-    }
-}
-
 allprojects {
     repositories {
         google()
